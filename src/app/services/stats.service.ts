d20d0f6e40db60c7f379a7dfb925387ef678e995
Sunil <charliebrown5414@gmail.com>
2019-06-19 12:48:30 -0700

    latest

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -12,10 +12,12 @@ export class StatsService {
 
   setStats(stats) {
     var keys = Object.keys(stats);
+    var aov7 = stats['aov7'];
     for (let i = 0; i < keys.length; ++i) {
       let key = keys[i];
       this.stats[key] = `$${parseInt(stats[key]).toLocaleString()}`;
     }
+    this.stats['aov7'] = `$${parseFloat(aov7).toFixed(2)}`;
   }
 
   getStats() {
